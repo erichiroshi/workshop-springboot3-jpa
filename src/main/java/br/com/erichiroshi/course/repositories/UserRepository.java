bce083801e6f207ed0f82c671776bb7821165fc5
erichiroshi <erichiroshi@gmail.com>
2023-02-21 14:34:04 -0400

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/br/com/erichiroshi/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package br.com.erichiroshi.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.erichiroshi.course.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
