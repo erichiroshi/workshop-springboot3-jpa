0345f3c9c6035112179dba44f4cfd4ba0763779b
erichiroshi <erichiroshi@gmail.com>
2023-02-21 14:18:36 -0400

    H2 database, test profile, JPA

--- src/main/java/br/com/erichiroshi/course/entities/User.java
+++ src/main/java/br/com/erichiroshi/course/entities/User.java
@@ -2,6 +2,11 @@ package br.com.erichiroshi.course.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,10 +18,15 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String email;
 	private String phone;
